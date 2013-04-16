a1b440b179fa0abca8df1e050b91647f1a1ec872
canburakt <canbu@canbu.info>
2013-04-16 13:11:48 -0430

    Maps üzerinde Marker düzenlendi. UsesPermission.Call_Phone eklendi.

--- IstanbulHackerspaceApp/src/org/istanbulhs/istanbulhackerspaceapp/pagefragments/HackerspaceMapFragment.java
+++ IstanbulHackerspaceApp/src/org/istanbulhs/istanbulhackerspaceapp/pagefragments/HackerspaceMapFragment.java
@@ -1,13 +1,22 @@
 package org.istanbulhs.istanbulhackerspaceapp.pagefragments;
 
+import org.istanbulhs.istanbulhackerspaceapp.R;
+
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,8 +40,21 @@ public class HackerspaceMapFragment extends SupportMapFragment {
 		getMap().moveCamera(update);
 		
 		MarkerOptions mrkr = new MarkerOptions();
-		mrkr.position(hsLocation);
-		getMap().addMarker(mrkr);
+		mrkr.position(hsLocation)
+			.title("İstanbul HackerSpace")
+			.snippet("Click to call - Tel : +90 216 418 04 17")
+			.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher));
+		
+		Marker hsMarker = getMap().addMarker(mrkr);
+		
+		getMap().setOnInfoWindowClickListener(new OnInfoWindowClickListener() {
+	        public void onInfoWindowClick(final Marker marker) {
+        		Intent callIntent = new Intent(Intent.ACTION_CALL);
+		        callIntent.setData(Uri.parse("tel:+90 216 418 04 17"));
+		        startActivity(callIntent);
+	        }
+		});
+	
 		
 		getMap().setMyLocationEnabled(true);
 		
